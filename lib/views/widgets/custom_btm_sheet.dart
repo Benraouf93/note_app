71031499b12db086b69b932c410291e4569675ce
Benraouf93 <snowdenhk93@gmail.com>
2025-01-31 22:25:46 +0100

    create add button note and finish bottom sheet

--- lib/views/widgets/custom_btm_sheet.dart
+++ lib/views/widgets/custom_btm_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_textfiled.dart';
+import 'custom_button.dart';
 
 class CustomBtmSheet extends StatelessWidget {
   const CustomBtmSheet({super.key});
@@ -7,16 +8,21 @@ class CustomBtmSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextFiled( hint: 'Title'),
-          SizedBox(height: 32),
-          CustomTextFiled( hint: 'Content', maxLines: 5),
-          SizedBox(height: 32),
-        ],
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: const Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextFiled( hint: 'Title'),
+            SizedBox(height: 18),
+            CustomTextFiled( hint: 'Content', maxLines: 5),
+            SizedBox(height: 100),
+            CustomButton(),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
 }
+
