bea35205323fb42a16ec0d36babf5b9f95292501
Benraouf93 <snowdenhk93@gmail.com>
2025-02-01 21:32:48 +0100

    add custom app bar to edit view

--- lib/views/widgets/custom_search_icon.dart
+++ lib/views/widgets/custom_search_icon.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
+  const CustomSearchIcon({super.key, required this.icon});
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width : 50,
-      height : 50,
-      decoration : BoxDecoration(
-        color : const Color.fromRGBO(255, 255, 255, 0.07),
-        borderRadius : BorderRadius.circular(12),
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(255, 255, 255, 0.07),
+        borderRadius: BorderRadius.circular(12),
       ),
-      child: const Center(
-        child: Icon(Icons.search, size: 28),
+      child: IconButton(
+        icon: Icon(
+          icon,
+          size: 28,
+        ),
+        onPressed: () {},
       ),
     );
   }
