12b3cf5d02c520ec46306826dc2c11477b1e090e
Benraouf93 <snowdenhk93@gmail.com>
2025-02-02 15:36:56 +0100

    edit main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/notes_view.dart';
-
-import 'cubits/add_note_cubit/add_note_cubit.dart';
 import 'simple_bloc_observer.dart';
 import 'views/constants.dart';
 
